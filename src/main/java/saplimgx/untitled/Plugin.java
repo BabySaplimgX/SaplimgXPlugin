3c2904d59d91e6a2ab8231c1ff1814c246138923
BabySaplimgX <Chikkienuggtz@gmail.com>
2023-03-17 10:01:43 +0100

    cvfbx

--- src/main/java/saplimgx/untitled/Plugin.java
+++ src/main/java/saplimgx/untitled/Plugin.java
@@ -1,13 +1,13 @@
 package saplimgx.untitled;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import saplimgx.untitled.handlers.TorchHandler;
 
 public final class Plugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        new TorchHandler(this);
     }
 
     @Override
---
+++ src/main/java/saplimgx/untitled/commands/Fly.java
@@ -0,0 +1,4 @@
+package saplimgx.untitled.commands;
+
+public class Fly {
+}
